48f2b43db1263f44df095322716bfdf467d7d27f
Anders <kiss7medical@gmail.com>
2019-05-29 15:58:36 +0200

    add React components for gif && gif_list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 
 import SearchBar from './searchbar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
 	render() {
+		const gifs = [
+			{ id: "2es7KiuoYLR3d5ye5v"},
+			{ id: "l4q7TIW8nEZYOJUf6"}
+		];
+
 		return (
 			<div>
 				<div className="left-scene">
 					<SearchBar />
+					<div className="selected-gif">
+						<Gif id="2es7KiuoYLR3d5ye5v" />
+					</div>
 				</div>
-				<div className="right-scene">
 
+				<div className="right-scene">
+					<GifList gifs={gifs} />
 				</div>
 			</div>	
-			);
+		);
 	}
 }
 
